17ad22506e6c90e213824acb9caa28d0661d544e
contact@dorin.info <contact@dorin.info>
2019-04-14 12:09:43 +0300

    adding some reference

--- springWebContentMVC/src/main/java/servingWebContent/Main.java
+++ springWebContentMVC/src/main/java/servingWebContent/Main.java
@@ -2,7 +2,9 @@ package servingWebContent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+link: https://spring.io/guides/gs/serving-web-content/
+ */
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
